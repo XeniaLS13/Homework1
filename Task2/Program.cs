6e57de89433809e040cba859d7596e5d568fec42
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-08 15:42:58 +0300

    Добавлен код для решения задания 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 22;
+int numberB = 3;
+int numberC = 9;
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max =");
+Console.WriteLine(max);
